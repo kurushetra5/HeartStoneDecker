7a8194cc8c3aa6da22ce3b24316cd6ec40bfa514
kuru <kurushetra5@gmail.com>
2018-03-10 20:18:25 +0100

    Initial Commit

---
+++ HeartStoneDecker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HeartStoneDecker
+//
+//  Created by Kurushetra on 10/3/18.
+//  Copyright © 2018 Kurushetra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
